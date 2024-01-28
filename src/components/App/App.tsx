ffcd478790d52d8e1553edac788b2d0e9e99367a
a.ivashov <a.ivashov@okko.tv>
2024-01-28 19:18:40 +0300

    Установил gh-pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import { Header } from '../Header/Header'
 import { PlanScreen } from '../PlanScreen/PlanScreen'
